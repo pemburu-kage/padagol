5b3b36eaf991b068c4f3b24d1e5c0523fd19be6e
pemburu-kage <angkaterbaik9@gmail.com>
2020-11-06 06:46:59 +0800

    Handle Add To Cart Button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
@@ -16,8 +17,9 @@ function App() {
               </div>
           </header>
           <main>
-              <Route path="/product/:id/" component={ProductScreen}></Route>
-              <Route path="/" component={HomeScreen} exact></Route>
+            <Route path="/cart/:id?" component={CartScreen}></Route>
+            <Route path="/product/:id/" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
           </main>
           <footer className="row center">All right reserved</footer>
       </div>
