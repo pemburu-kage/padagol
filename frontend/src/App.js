5f7d654231e4f84b4deb2f1d5f9d6a21a6e071f2
pemburu-kage <angkaterbaik9@gmail.com>
2020-11-07 20:02:44 +0800

    Design Signin Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -28,6 +29,7 @@ function App() {
           <main>
             <Route path="/cart/:id?" component={CartScreen}></Route>
             <Route path="/product/:id/" component={ProductScreen}></Route>
+            <Route path="/signin" component={SigninScreen}></Route>
             <Route path="/" component={HomeScreen} exact></Route>
           </main>
           <footer className="row center">All right reserved</footer>
