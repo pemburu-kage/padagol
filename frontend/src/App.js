4bd227009991dbe1547dac5a89399ca6f92eed80
pemburu-kage <angkaterbaik9@gmail.com>
2020-11-12 06:33:40 +0800

    Creat Admin View

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -61,7 +61,28 @@ function App() {
                 ) : (
                   <Link to="/signin">Sign In</Link>
                 )}
-                </div>
+                {userInfo && userInfo.isAdmin && (
+                  <div className="dropdown">
+                    <Link to="#admin">
+                      Admin <i className="fa fa-caret-down"></i>
+                    </Link>
+                    <ul className="dropdown-content">
+                      <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                      </li>
+                      <li>
+                        <Link to="/productlist">Products</Link>
+                      </li>
+                      <li>
+                        <Link to="/orderlist">Orders</Link>
+                      </li>
+                      <li>
+                        <Link to="/userlist">Users</Link>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
           </header>
           <main>
             <Route path="/cart/:id?" component={CartScreen}></Route>
